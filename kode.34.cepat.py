be4a32dec790fb7cef47b1ebba9e90339ae93d1b
Gesa Rizky Nugraha <109656432+gesarizky@users.noreply.github.com>
2022-10-12 14:10:11 +0700

    Add files via upload

---
+++ kode.34.cepat.py
@@ -0,0 +1,15 @@
+# Author  : Gesa Rizky Nugraha
+# Email   : gesarizkynugraha@gmail.com
+# Website : karenabelajar.blogspot.com
+
+# Menginput
+jarak = float(input("Tuliskan Berapa Kilometer Jarak Temput: "))
+waktu = float(input("Tuliskan Berapa Jam Yang dibutuhkan: "))
+
+# Mengkonversi
+cepat = jarak / waktu
+
+# Menampilkan Hasil
+print('==========================================================')
+print('Maka Kecepatan = %0.2f kilometer/Jam'  %(cepat))
+print('==========================================================')
